30d4e93728a4ad0a477d5923368235aa251b63cc
Liaksej <tarusov.liaksej@gmail.com>
2023-10-12 17:38:58 +0200

    Add Vercel KV as storage for notes API
    The notes API was refactored to use Vercel KV for storage. This improves efficiency by removing the need for a local storage variable. Also included changes to align with the CORS policy by moving the headers to vercel.json, and added OPTIONS method to handle preflight requests. Furthermore, a `build` script in package.json has been updated, HTTP methods in hands.http for notes operation changed, and .env.development.local file added to .gitignore.

--- api/notes.ts
+++ api/notes.ts
@@ -1,42 +1,43 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 import { v4 as uuidv4 } from "uuid";
+import { kv } from "@vercel/kv";
 
 type Note = {
   id: string;
   content: string;
 };
 
-export default function (request: VercelRequest, response: VercelResponse) {
-  response.setHeader("Access-Control-Allow-Origin", "*");
-  response.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, DELETE",
-  );
-  response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+export default async function (
+  request: VercelRequest,
+  response: VercelResponse,
+) {
+  console.log(`Received ${request.method} request`);
+
+  if (request.method === "OPTIONS") {
+    return response.status(200).end();
+  }
+
+  let notes: Note[] = (await kv.get("notes")) || [];
 
   switch (request.method?.toUpperCase()) {
     case "GET":
-      return response.status(200).json(sendNotes());
+      return response.status(200).json(await kv.get("notes"));
     case "POST":
-      notesArray.push({
+      notes.push({
         id: uuidv4(),
         content: request.body.content,
       });
-      return response.status(201).json(notesArray.at(-1));
+      await kv.set("notes", notes);
+      return response.status(201).json(notes.at(-1));
     case "DELETE":
       if (!request.query.id) {
         return response.status(400).send("No note id provided");
       }
-      notesArray = notesArray.filter((note) => note.id !== request.query.id);
+      notes = notes.filter((note: Note) => note.id !== request.query.id);
+      await kv.set("notes", notes);
       return response.status(200).send("Note deleted");
     default:
       return response.status(405).send("Not allowed");
   }
-}
-
-let notesArray: Note[] = [];
-
-function sendNotes() {
-  return notesArray;
 }
